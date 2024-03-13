fb411e1e0078906da59919f1f1482203c4897692
denis <kirgizov03.88@gmail.com>
2024-03-13 17:37:23 +0300

    fix simple page

--- src/Controller/SimplePageController.php
+++ src/Controller/SimplePageController.php
@@ -8,25 +8,25 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SimplePageController extends AbstractController
 {
-    #[Route('/services', name: 'services')]
+    #[Route('/services/', name: 'services')]
     public function services(): Response
     {
         return $this->render('services/index.html.twig');
     }
 
-    #[Route('/price_list', name: 'price_list')]
+    #[Route('/price_list/', name: 'price_list')]
     public function priceList(): Response
     {
         return $this->render('static_page/price_list.html.twig');
     }
 
-    #[Route('/contacts', name: 'contacts')]
+    #[Route('/contacts/', name: 'contacts')]
     public function contacts(): Response
     {
         return $this->render('static_page/contacts.html.twig');
     }
 
-    #[Route('/about', name: 'about')]
+    #[Route('/about/', name: 'about')]
     public function about(): Response
     {
         return $this->render('static_page/about.html.twig');
